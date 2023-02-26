fbd0d0a16e536af916fc3d5d933af83430d1cea8
Vera Maria <mymadonna89@mail.ru>
2023-02-26 20:33:59 +0600

    Homework 38 solved

--- Homework_38/Program.cs
+++ Homework_38/Program.cs
@@ -1,3 +1,27 @@
-﻿// Задача 38: Задайте массив вещественных чисел. 
-//Найдите разницу между максимальным и минимальным элементов массива.
-//[3 7 22 2 78] -> 76
+﻿using System.Linq;
+namespace homework
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] Segment = new int [10];
+
+            Random rand = new Random();
+            for (int i = 0; i < Segment.Length; i++)
+            {
+                Segment [i] = rand.Next(1, 99);
+               System.Console.WriteLine($"Заполнение массива {i}\t" + Segment[i]);
+                
+            }
+                 System.Console.WriteLine("Максимальный элемент массива:\t" + Segment.Max());
+                 System.Console.WriteLine("Минимальный элемент масссива:\t" + Segment.Min());
+                 System.Console.WriteLine( "Разница:\t" + (Segment.Max()-Segment.Min()));
+        }
+
+    }
+
+}
+
+
+
